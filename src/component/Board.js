2d7323fbe62823a1ee0e60717ba1b39fc67bd669
shimanamisan <mikan.sup.all@gmail.com>
2020-11-11 23:25:51 +0900

    update

--- src/component/Board.js
+++ src/component/Board.js
@@ -2,53 +2,58 @@ import React from 'react';
 import Square from './Square'
 
 export default class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            // fillメソッドは指定した開始から終了インデックスまでの値を変更した配列を返す
-            // ここでは開始・終了インデックスを指定していないので、配列の値が全て null の配列が返ってくる
-            squares: Array(9).fill(null)
-        }
-    }
+    // Gameコンポーネントに状態管理を移動した
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         // fillメソッドは指定した開始から終了インデックスまでの値を変更した配列を返す
+    //         // ここでは開始・終了インデックスを指定していないので、配列の値が全て null の配列が返ってくる
+    //         squares: Array(9).fill(null),
+    //         xIsNext: true,
+    //     }
+    // }
 
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        squares[i] = "X";
-        this.setState( {squares: squares })
-    }
+    // handleClick(i) {
+    //     const squares = this.state.squares.slice();
+    //     if (calculateWinner(squares) || squares[i]) {
+    //       return;
+    //     }
+    //     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //     this.setState({
+    //       squares: squares,
+    //       xIsNext: !this.state.xIsNext,
+    //     });
+    //   }
 
     renderSquare(i) {
       // JavaScript が return の後にセミコロンを挿入するのを防ぐため、カッコを付け加えている
       return (
             <Square 
-                value={this.state.squares[i]} 
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]} 
+                thatClick={() => this.props.propClick(i)}
             />
             );
     }
   
     render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+        return (
+          <div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+            </div>
           </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
+        );
     }
-  }
+}
--- src/component/Game.js
+++ src/component/Game.js
@@ -2,18 +2,98 @@ import React from 'react';
 import Board from './Board'
 
 export default class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+    constructor(props){
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        }
+
     }
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          history: history.concat([{
+            squares: squares,
+          }]),
+          xIsNext: !this.state.xIsNext,
+        });
+      }
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        const moves = history.map((step, move) => {
+            console.log("これがstepで配列を分解した後のアイテム ：" + JSON.stringify(step) + " これがmove（indexの値？）："  + move)
+            const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+          return (
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+          );
+        });
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+    
+        return (
+          <div className="game">
+            <div className="game-board">
+              <Board
+                squares={current.squares}
+                propClick={(i) => this.handleClick(i)}
+              />
+            </div>
+            <div className="game-info">
+              <div>{status}</div>
+              <ol>{moves}</ol>
+            </div>
+          </div>
+        );
+      }
   }
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    console.log(lines)
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
