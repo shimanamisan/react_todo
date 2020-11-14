f37e854cb368e29421ea3e8e4512dfc917b9c943
shimanamisan <mikan.sup.all@gmail.com>
2020-11-15 07:51:46 +0900

    アイコンを追加

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -5,14 +5,14 @@ export default class Icon extends React.Component {
         super(props)
         this.state = {
             url: this.props.url,
-            icon: this.props.iconPath
+            iconPath: this.props.path
         }
     }
 
     render (){
         return (
             <div>
-              <a href={this.state.url} target="_blank" rel="noopener"><img className="p-logo" src={this.state.icon} alt="TODOリストのロゴ" /></a>
+              <a href={this.state.url} target="_blank" rel="noopener"><img className="p-logo" src={this.state.iconPath} alt="TODOリストのロゴ" /></a>
             </div>
         );
     }
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ClassNames from 'classnames';
 import Icon from '../common/Icon'
 import vueicon from '../../img/vue_logo.svg'
+import jqueryicon from '../../img/jquery_logo.svg'
+import reacticon from '../../img/react_logo.svg'
 
 export default class Modal extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            vueUrl: "https://vue-task-app.shimanamisan.com",
+            jQueryUrl: "https://jq-task-app.shimanamisan.com",
+            reactUrl:""
+        }
         // このコンポーネント内で使うメソッドのthisの向き先を、このクラスに限定する
         this.handleModal = this.handleModal.bind(this);
     }
@@ -29,19 +36,28 @@ export default class Modal extends React.Component {
                     <div className="close icon" onClick={this.handleModal}></div>
                     <div className="c-modal__body">
                     <li className="c-modal__body__list">
-                        <Icon/>
+                        <Icon 
+                        path={vueicon}
+                        url={this.state.vueUrl}
+                        />
                         <div>
                         <p>Vue.js todo list</p>
                         </div>
                     </li>
                     <li className="c-modal__body__list">
-                        {/* <IconLogo :icon-path="this.jquery_path" /> */}
+                        <Icon 
+                            path={jqueryicon}
+                            url={this.state.jQueryUrl}
+                        />
                         <div>
                         <p>jQuery todo list</p>
                         </div>
                     </li>
                     <li className="c-modal__body__list">
-                        {/* <IconLogo :icon-path="this.react_path" /> */}
+                        <Icon 
+                            path={reacticon}
+                            url={this.state.reactUrl}
+                        />
                         <div>
                         <p>React.js todo list</p>
                         </div>
